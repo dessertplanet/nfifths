48bd073b0091bcfd0918881c96eb36ca1ee12755
Dune Desormeaux <dune.desormeaux@gmail.com>
2024-11-30 19:19:02 -0800

    fifths and dots working, env and slew prototypes

--- nfifths.lua
+++ nfifths.lua
@@ -4,30 +4,153 @@
 
 engine.name = 'PolyPerc'
 music = require 'musicutil'
-m = midi.connect()
+midi_in = midi.connect(1)
+midi_out = midi.connect(2)
 
 -- tonics on the circle of fifths starting from Gb all the way to F#,
 -- always choosing an octave with a note as close as possible to the key center (0)
-circle_of_fifths = { -6, 1, -4, 3, -2, 5, 0, -5, 2, -3, 4, -1, 6 }
+circle_of_fifths = { 0, -5, 2, -3, 4, -1, 6, -6, 1, -4, 3, -2, 5 }
+
+key_names = {
+  "C",
+  "G",
+  "D",
+  "A",
+  "E",
+  "B",
+  "F#",
+  "Db",
+  "Ab",
+  "Eb",
+  "Bb",
+  "F"
+}
+
+-- window boundaries for 13 equal-ish sized windows for -5 to +5V (for crow input)
+thirteen_windows = { -4.97, -4.5, -3.5, -2.5, -1.5, -0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 4.97 }
+
+voicing = 1
+
+permutations = {
+  { true,  false, false },
+  { false, true,  false },
+  { false, false, true },
+  { true,  true,  false },
+  { true,  false, true },
+  { false, true,  true },
+  { true,  true,  true }
+}
 
 tonic = 60
-key_index = 7
+key_index = 1
 
 function init()
   scale = major_scale(tonic)
   engine.release(1)
+  screen.aa(0)
+
+  --crow init steps
+  --input 2 produces value 1 at -5V and value 12 at +5V
+  crow.input[2].mode('window', thirteen_windows, 0.2)
+
+  --quantize all tuned outputs to the chromatic scale in case our fifths get weird
+  for i = 1, 3 do
+    crow.output[i].scale({})
+  end
+  --comment
+
+  --start in C major (ie. 0V at input 2)
+  crow.input[2].window(key_index)
+
+  -- set up output 4 pulses
+  crow.output[4].action = crow.pulse(0.01, 5)
+
+  redraw()
 end
 
-m.event = function(data)
+midi_in.event = function(data)
   local msg = midi.to_msg(data)
-  if (msg.type == 'note_on') then
+  if msg.type == 'note_on' then
     local chord = quant_chord(msg.note)
     for i = 1, 3 do
-      engine.hz(music.note_num_to_freq(chord[i]))
+      if permutations[voicing][i] then
+        engine.hz(music.note_num_to_freq(chord[i]))
+        midi_out:note_on(chord[i], msg.vel)
+      end
+    end
+  elseif msg.type == 'note_off' then
+    local chord = quant_chord(msg.note)
+    for i = 1, 3 do
+      midi_out:note_off(chord[i])
     end
   end
-  if msg.type == 'note_off' then
-    engine.amp(0)
+end
+
+function redraw()
+  screen.clear()
+  draw_fifths()
+  draw_dots(permutations[voicing])
+  draw_env()
+  draw_slew()
+  screen.update()
+end
+
+function draw_slew()
+  screen.move(85,53)
+  screen.text("slew: 30%")
+end
+
+function draw_env()
+  screen.move(85,38)
+  screen.line(85,28)
+  screen.line(95,28)
+  screen.line(95,38)
+  screen.stroke()
+end
+
+function draw_dots(states)
+  local dot_radius = 1
+  local x = 80
+  local y = 15
+  local spacing = 8
+
+  for i = 1, 3 do
+    if states[i] then
+      screen.move(x + (i * spacing), y)
+      screen.circle(x + (i * spacing), y, dot_radius + 1)
+    else
+      screen.move(x + dot_radius + (i * spacing), y)
+      screen.pixel(x + (i * spacing), y)
+    end
+    screen.fill()
+    screen.stroke()
+  end
+end
+
+function draw_fifths()
+  local radians = 0
+  local center_x = 32
+  local center_y = 34
+  local big_radius = 25
+  local small_radius = 7
+  local x
+  local y
+
+  for i = 1, 13 do
+    x = center_x + (big_radius * math.sin(radians))
+    y = center_y - (big_radius * math.cos(radians))
+
+    if i < 13 then
+      radians = radians + (math.pi / 6)
+      screen.move(x, y)
+      screen.text_center(key_names[i])
+    end
+
+    if i == key_index then
+      screen.move(x + 1+ small_radius, y-2)
+      screen.circle(x+1, y - 2, small_radius)
+      screen.stroke()
+    end
   end
 end
 
@@ -37,12 +160,6 @@ function quant_chord(note)
   local octave_delta = (math.floor(note / 12) * 12) - tonic
   local root = tonic +
       (scale[closest_scale_degree + 1] + (math.floor(closest_scale_degree / 7) * 12) - tonic) + octave_delta
-
-  -- if the note isn't in the key, randomly choose between the next or previous note from the key
-  -- guitarists know: a bum note is always only one fret (semitone) away from a good note. Either way will work.
-  if note ~= root then
-    closest_scale_degree = math.floor(closest_scale_degree - (math.random() * (root - note)) + 0.5)
-  end
 
   local nums = {}
 
@@ -87,48 +204,39 @@ function key(n, z)
     elseif n == 3 then
       rotate_key(1)
     end
+    redraw()
   end
 end
 
------ BELOW HERE FROM CROW VERSION
----
----
----
+function enc(n, d)
+  --print("n = ",n,"d = ",d)
+  if n == 1 then
+    voicing = voicing + d
+    if voicing > 7 then
+      voicing = 1
+    elseif voicing < 1 then
+      voicing = 7
+    end
+    redraw()
+  elseif n == 2 then
 
---[[
--- window boundaries for 13 equal-ish sized windows for -5 to +5V
-thirteen_windows = { -4.97, -4.5, -3.5, -2.5, -1.5, -0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 4.97 }
+  elseif n == 3 then
+
+  end
+end
+
 -- choose output values based on input 1 and offsets based on fifths. 0V is assumed to be tuned to C3 (not mandatory!)
-input[1].scale = function(x)
+crow.input[1].scale = function(x)
   local fifth = 7 / 12
   local relative_to_c3 = x.volts + 2
-  output[1].volts = relative_to_c3
-  output[2].volts = relative_to_c3 + fifth
-  output[3].volts = relative_to_c3 + fifth + fifth
-  output[4]() -- pulse output 4 when tuning
+  crow.output[1].volts = relative_to_c3
+  crow.output[2].volts = relative_to_c3 + fifth
+  crow.output[3].volts = relative_to_c3 + fifth + fifth
+  crow.output[4]() -- pulse output 4 when tuning
 end
 
 -- when input 2 hops between windows, choose a new key from the circle of fifths
-input[2].window = function(x)
+crow.input[2].window = function(x)
   local new_key = major_scale(circle_of_fifths[x])
-  input[1].mode('scale', new_key)
+  crow.input[1].mode('scale', new_key)
 end
-
---initialize things
-function init()
-
-  --input 2 produces value 1 at -5V and value 12 at +5V
-  input[2].mode('window',thirteen_windows,0.2)
-
-  --quantize all tuned outputs to the chromatic scale in case our fifths get weird
-  for i=1,3 do
-      output[i].scale({})
-  end
-
-  --start in C major (ie. 0V at input 2)
-  input[2].window(7)
-
-  -- set up output 4 pulses
-  output[4].action = pulse(0.01, 5)
-end
---]]
